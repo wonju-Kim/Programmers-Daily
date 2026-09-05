65741fad42323c6132eeaed0d15f0d5956c17ffe
wonju-Kim <wonju056@gmail.com>
2026-09-05 12:49:18 +0900

    Solve: 배열 뒤집기

---
+++ Programmers/C/배열 뒤집기.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int num_list[], size_t num_list_len) {
+    int* answer = (int*)malloc(sizeof(int)*num_list_len);
+    for(int i; i < num_list_len; i++){
+        answer[i] = num_list[num_list_len - i - 1];
+    }
+    return answer;
+}
