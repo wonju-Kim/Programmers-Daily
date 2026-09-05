1d71eea686dcacaf088e76ed2c16e476932e2f0c
wonju-Kim <wonju056@gmail.com>
2026-09-05 12:54:24 +0900

    Sovle: 배열 뒤집기.c

--- Programmers/C/배열 뒤집기.c
+++ Programmers/C/배열 뒤집기.c
@@ -4,7 +4,7 @@
 
 int* solution(int num_list[], size_t num_list_len) {
     int* answer = (int*)malloc(sizeof(int)*num_list_len);
-    for(int i; i < num_list_len; i++){
+    for(size_t i = 0; i < num_list_len; i++){
         answer[i] = num_list[num_list_len - i - 1];
     }
     return answer;
